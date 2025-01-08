ab948c63b4f51dec5a3f8ba34e04d7b50682068f
jKrithikaa <140594928+jKrithikaa@users.noreply.github.com>
2025-01-08 13:52:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-
-// Components for each section
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Testimonials from './components/Testimonials';
-import ContactForm from './components/ContactForm';
+import Testimonials from './components/Testimonial';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
